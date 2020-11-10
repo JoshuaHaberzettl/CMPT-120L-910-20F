61cc83bae1cafebafed8342794c04faece1bf933
JoshuaHaberzettl <35841103+JoshuaHaberzettl@users.noreply.github.com>
2020-11-09 23:11:50 -0500

    Update application.py

--- SemesterProject/application.py
+++ SemesterProject/application.py
@@ -1,6 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+#@app.route('/')
+#def hello_world():
+#    return 'This is a test.'
+
+from flask import render_template
+
 @app.route('/')
-def hello_world():
-    return 'This is a test.'
+#@app.route('/hello/')
+#@app.route('/hello/name')
+def hello(name='Josh'):
+    return render_template('hello.html', name=name)
