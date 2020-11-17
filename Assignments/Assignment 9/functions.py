39200c24d2be4e132c4f5c739e2540db86d590a9
JoshuaHaberzettl <35841103+JoshuaHaberzettl@users.noreply.github.com>
2020-11-16 19:22:13 -0500

    Create functions.py

---
+++ Assignments/Assignment 9/functions.py
@@ -0,0 +1,11 @@
+import Calculator
+
+calc = Calculator.Calculator()
+
+print(calc.addition(1,calc.negate(2)))
+
+print(calc.multiplication(calc.subtraction(4,1),calc.division(10,2)))
+
+print(calc.exponent(2,8))
+
+print(calc.square_root(144))
