8ca592b12577ba4b91b06155978974d83c4e4ac0
JoshuaHaberzettl <35841103+JoshuaHaberzettl@users.noreply.github.com>
2020-11-25 01:20:55 -0500

    Update application.py

--- SemesterProject/application.py
+++ SemesterProject/application.py
@@ -7,12 +7,14 @@ logging = create_logger(app)
 
 @app.route('/')
 def hello_world():
+    logging.info('default test page')
     return 'This is a test.'
 
 
 @app.route('/hello/')
 @app.route('/hello/name')
 def hello(name='Josh'):
+    logging.info('page with url extention to test templates')
     return render_template('hello.html', name=name)
 
 #example log calls
