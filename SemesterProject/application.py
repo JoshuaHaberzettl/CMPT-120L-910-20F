b88c0641c4fe62040d6a456f5b1a900848465b9d
JoshuaHaberzettl <35841103+JoshuaHaberzettl@users.noreply.github.com>
2020-11-25 01:16:37 -0500

    Update application.py

--- SemesterProject/application.py
+++ SemesterProject/application.py
@@ -1,13 +1,21 @@
 from flask import Flask
+from flask import render_template
+from flask.logging import create_logger
 app = Flask(__name__)
+logging = create_logger(app)
+
 
 @app.route('/')
 def hello_world():
     return 'This is a test.'
 
-from flask import render_template
 
 @app.route('/hello/')
 @app.route('/hello/name')
 def hello(name='Josh'):
     return render_template('hello.html', name=name)
+
+#example log calls
+logging.debug('A value for debugging')
+logging.warning('A warning occurred (%d apples)', 42)
+logging.error('An error occurred')
