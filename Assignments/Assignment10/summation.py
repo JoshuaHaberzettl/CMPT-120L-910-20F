de27376b2a852178462d55d52e82707c121bcdf2
JoshuaHaberzettl <35841103+JoshuaHaberzettl@users.noreply.github.com>
2020-11-26 03:23:56 -0500

    Update summation.py

--- Assignments/Assignment10/summation.py
+++ Assignments/Assignment10/summation.py
@@ -1,13 +1,14 @@
 import logging
 import argparse
 parser = argparse.ArgumentParser(description="Displays the summation notation of a given number using logging.")
-
+logging.basicConfig(format="%(message)s", level=logging.INFO)
 
 #You wrote that this was considered poor practice, but didn't give an
 #example of good practice for a positional argument, just an optional one
 parser.add_argument("number", help="This is the number to be summed")   
 args = parser.parse_args()
 
+logging.info('The number recieved in argparse is: ' + args.number)
 
 #Finds and returns the summation notation for the given number through recursion
 def summation(number):
@@ -23,6 +24,4 @@ def summation_recursive(number, current_total):
 
 
 #Print using the logger
-logging.basicConfig(format="%(message)s", level=logging.INFO)
-logging.info('The number recieved in argparse is: ' + args.number)
 logging.info("The summation notation of %s is: %s.", args.number, summation(int(args.number)))
